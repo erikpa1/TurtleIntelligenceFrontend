90c70f88eb84ad98cb957e6302a337c18e3f9e05
exevrs <140792984+exevrs@users.noreply.github.com>
2025-05-24 16:26:47 +0200

    Update prototype of RuntimeActorFiber

---
+++ src/TurtleApp/Routes/WorldDock/Submodules/RuntimeActorFiber.tsx
@@ -0,0 +1,16 @@
+export default function RuntimeActorFiber({actor}) {
+
+    function refreshFn(newState) {
+
+    }
+
+    return [
+        refreshFn,
+        (
+            <group>
+
+            </group>
+        )
+    ]
+
+}
