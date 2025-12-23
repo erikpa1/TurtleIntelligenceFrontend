75b3660234f8e6ec09447d04c46cec551a4e61ed
Erik Palencik <erik.palencik@outlook.com>
2025-12-23 17:54:45 +0100

    Update llm attributes

--- src/Turtle/LLM/LLMAgentsDock/Edit/EditViews/COUWriteToFileView.tsx
+++ src/Turtle/LLM/LLMAgentsDock/Edit/EditViews/COUWriteToFileView.tsx
@@ -1,11 +1,9 @@
 import AgentNodeParent from "@Turtle/LLM/LLMAgentsDock/Data/Nodes/AgentNodeParent";
 import {Form} from "antd";
-import SelectHttpMethod from "@TurtlePostman/Components/SelectHttpMethod";
-import {HttpTriggerData} from "@Turtle/LLM/LLMAgentsDock/Data/Nodes/HttpTriggerData";
+
 import {WriteToFileNode} from "@Turtle/LLM/LLMAgentsDock/Data/Nodes/WriteToFileNode";
 import StringAttributeView from "@Turtle/Components/Forms/StringAttributeView";
 import {BoolAttributeView} from "@Turtle/Components/Forms/BoolPropertyView";
-import FolderAttributeView from "@Turtle/Components/Forms/FolderPathAttributeView";
 
 interface COUWriteToFileViewProps {
     node: AgentNodeParent
