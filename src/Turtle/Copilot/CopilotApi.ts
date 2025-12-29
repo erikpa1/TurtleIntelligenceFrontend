b7068c29b4555d3e64a430950f99b8ada6cb3a92
Erik Palencik <erik.palencik@outlook.com>
2025-12-29 14:42:12 +0100

    Update copilot architecture

--- src/Turtle/Copilot/CopilotApi.ts
+++ src/Turtle/Copilot/CopilotApi.ts
@@ -4,9 +4,12 @@ import Turxios from "@Turtle/Api/Turxios"
 export default class CopilotApi {
 
     static async ChatCopilotContext(context: string, question: string): Promise<string> {
-        const response = (await Turxios.post(`/api/${context}/copilot`, {
-            data: question
-        })).data
+        const response = (await Turxios.post(`/api/${context}/copilot`, question)).data
+        return response
+    }
+
+    static async GetCopilotExample(context: string): Promise<string> {
+        const response = (await Turxios.post(`/api/${context}/copilot/examples`)).data
 
         return response
     }
--- src/Turtle/Copilot/CopilotChatBubble.tsx
+++ src/Turtle/Copilot/CopilotChatBubble.tsx
@@ -4,10 +4,16 @@ import React from "react"
 import {RightSubmitButton} from "@Turtle/Components/RightSubmitButton"
 import TurtleApp from "@TurtleApp/TurtleApp"
 import CopilotApi from "@Turtle/Copilot/CopilotApi"
+import UnderlineButton from "@Turtle/Components/UnderlineButton"
+import {useTurtleModal} from "@Turtle/Hooks/useTurtleModal"
+import {Example} from "@react-three/drei"
+import {TurtleSkeleton} from "@Turtle/Components/TurtleSkeleton"
 
 
 export default function CopilotChatBubble({context}) {
 
+
+    const {activate, deactivate} = useTurtleModal()
 
     const askContext = React.useMemo(() => {
         return {
@@ -15,20 +21,69 @@ export default function CopilotChatBubble({context}) {
         }
     }, [])
 
+    function showExamples() {
+        activate({
+            title: "examples",
+            content: (
+                <_Examples context={context}/>
+            )
+        })
+    }
+
+
     async function execute() {
         TurtleApp.Lock()
         await CopilotApi.ChatCopilotContext(context, askContext.question)
         TurtleApp.Unlock()
     }
 
+
+
+
     return (
         <Flex vertical>
+
             <StringAreaAttributeView
                 entity={askContext}
                 attribute={"question"}
             />
 
+            <UnderlineButton
+                lang={"examples"}
+                onClick={showExamples}
+            />
+
             <RightSubmitButton onClick={execute}/>
         </Flex>
     )
+}
+
+function _Examples({context}) {
+
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    const [htmlStr, setHtmlStr] = React.useState("")
+
+    async function refresh() {
+        setIsLoading(true)
+        setHtmlStr(await CopilotApi.GetCopilotExample(context))
+        setIsLoading(false)
+    }
+
+    React.useEffect(() => {
+        refresh()
+    }, [context])
+
+    if (isLoading) {
+        return (
+            <TurtleSkeleton/>
+        )
+    } else {
+        return (
+            <div dangerouslySetInnerHTML={{__html: htmlStr}}/>
+        )
+    }
+
+
+
 }
