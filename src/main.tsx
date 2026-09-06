5a6548c307d3a01d1d3cf3afd5075b28a3a1972e
Erik Palencik <erik.palencik@outlook.com>
2026-09-06 20:34:42 +0200

    Update new icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,30 @@
-import {StrictMode, Suspense} from 'react';
+import { StrictMode, Suspense } from "react";
 
-import "./index.css"
+import "./index.css";
 
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-import TurtleApp from './App';
+import TurtleApp from "./App";
 
+import "./i18n";
+import { HashRouter } from "react-router-dom";
 
-import "./i18n"
-import {HashRouter} from "react-router-dom";
+import { TurxiosProvider } from "@Turtle/Api/Turxios";
 
-import {TurxiosProvider} from "@Turtle/Api/Turxios";
-
-import {QueryClientProvider} from "react-query";
-import {TurtleQueryClient} from "@Turtle/TanStack";
+import { QueryClientProvider } from "react-query";
+import { TurtleQueryClient } from "@Turtle/TanStack";
 import TurtleThemeProvider from "../TurtleThemeProvider";
 
-const root = createRoot(document.getElementById('root') as any);
+const root = createRoot(document.getElementById("root") as any);
 
-import {App} from "antd";
-
+import { App, Button } from "antd";
+import IconFolderOpen from "@TurtleIcons/IconFolderOpen";
+import IconCamping from "@TurtleIcons/IconCamping";
+import IconCategory from "@TurtleIcons/IconCategory";
+import IconSearch from "@TurtleIcons/IconSearch";
+import IconStream from "@TurtleIcons/IconStream";
+import IconCalendar from "@TurtleIcons/IconCalendar";
+import IconScene from "@TurtleIcons/IconScene";
 
 root.render(
     <QueryClientProvider client={TurtleQueryClient}>
@@ -32,14 +37,21 @@ root.render(
                             v7_startTransition: true,
                         }}
                     >
-                        <TurxiosProvider/>
-                        <TurtleApp/>
-
+                        <TurxiosProvider />
+                        <TurtleApp />
                     </HashRouter>
                 </Suspense>
             </App>
         </TurtleThemeProvider>
+    </QueryClientProvider>,
+);
 
+root.render(
+    <>
+        <IconScene />
 
-    </QueryClientProvider>
+        <Button type="primary" icon={<IconScene />}>
+            Here
+        </Button>
+    </>,
 );
