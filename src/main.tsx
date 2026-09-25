47966b28e9ec22aa8ee65f99f138055a5fc79dd9
Erik Palencik <erik.palencik@outlook.com>
2026-09-25 11:58:36 +0200

    Update icons refactor

--- src/main.tsx
+++ src/main.tsx
@@ -16,24 +16,6 @@ import {QueryClientProvider} from "react-query";
 import {TurtleQueryClient} from "@Turtle/TanStack";
 import TurtleThemeProvider from "../TurtleThemeProvider";
 import {App} from "antd";
-import IconPause from "@TurtleIcons/IconPause";
-import IconCalendarClock from "@TurtleIcons/IconCalendarClock";
-import IconOrderApprove from "@TurtleIcons/IconOrderApprove";
-import IconBallot from "@TurtleIcons/IconBallot";
-import IconPrecisionManufacturing from "@TurtleIcons/IconPrecisionManufacturing";
-import IconEngineering from "@TurtleIcons/IconEngineering";
-import IconChronic from "@TurtleIcons/IconChronic";
-import IconFormAppScript from "@TurtleIcons/IconFormAppScript";
-import IconCalendarMonth from "@TurtleIcons/IconCalendarMonth";
-import IconDatabase from "@TurtleIcons/IconDatabase";
-import IconAccountBalance from "@TurtleIcons/IconAccountBalance";
-import IconRoutine from "@TurtleIcons/IconRoutine";
-import IconPriceChange from "@TurtleIcons/IconPriceChange";
-import IconGrid from "@TurtleIcons/IconGrid";
-import IconElevator from "@TurtleIcons/IconElevator";
-import IconLock from "@TurtleIcons/IconLock";
-import IconLockOpenRight from "@TurtleIcons/IconLockOpenRight";
-import IconLockOpen from "@TurtleIcons/IconLockOpen";
 
 const root = createRoot(document.getElementById('root') as any);
 
